d1c1f7a412300314d54ae4e7eb6a67e9b09a2d88
Max Timje <max@timje.se>
2018-08-27 09:52:46 +0200

    minor style change

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,5 @@
 // make connection
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://192.168.0.104:3000');
 
 // Get saved data from sessionStorage
 var storedUsername = sessionStorage.getItem('username');
